dee47b104b44a82b84c4f51223cc11f0c5569fd5
trainingport <trainingport74@gmail.com>
2017-10-28 20:55:29 +0530

    datadir, imagedir, start and end image number added

---
+++ src/com/avik/java/anniversary/constants/AnniversaryConstants.java
@@ -0,0 +1,12 @@
+package com.avik.java.anniversary.constants;
+
+public interface AnniversaryConstants {
+	
+	public static final String DEPLOYMENT_TYPE = "deploymenttype";
+	public static final String LOCAL_DATA_DIR = "localdatadir";
+	public static final String LOCAL_IMAGE_DIR = "localimagedir";
+	public static final String SERVER_DATA_DIR = "serverdatadir";
+	public static final String SERVER_IMAGE_DIR = "serverimagedir";
+	public static final String IMAGE_START_INDEX = "imagestartindex";
+	public static final String IMAGE_END_INDEX = "imageendindex";
+}
