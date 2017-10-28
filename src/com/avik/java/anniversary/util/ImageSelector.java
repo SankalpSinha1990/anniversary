0416586eb63680537df28b557dd794d857ced666
trainingport <trainingport74@gmail.com>
2017-10-28 20:57:55 +0530

    config props externalized

--- src/com/avik/java/anniversary/util/ImageSelector.java
+++ src/com/avik/java/anniversary/util/ImageSelector.java
@@ -1,21 +1,53 @@
 package com.avik.java.anniversary.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Random;
 
-public class ImageSelector {
-	
-	private int startFileNum = 1;
-	private int endFileNum = 6;
-	private String filePath = "C:\\devlopment\\anniversary\\images\\";
-	
-	public ImageSelector() {
-		
-	}
+import com.avik.java.anniversary.constants.AnniversaryConstants;
+
+public class ImageSelector extends AbstractSelector {
 	
 	public String getRandomImageFileName() {
 		
-		int fileNum = randInt(startFileNum, endFileNum);
-		String fileName = filePath + new Integer(fileNum).toString() + ".png";
+		String applicationHome = super.getApplicationHome();
+		String imageFileLocation = "";
+		Properties applicationProp = new Properties();
+		InputStream input = null;
+		int imageStartNum = 0;
+		int imageEndNum = 0;
+		try {
+			input = new FileInputStream(applicationHome+"application.properties");
+			applicationProp.load(input);
+			if(applicationProp.getProperty(AnniversaryConstants.DEPLOYMENT_TYPE).equalsIgnoreCase("local")) {
+				imageFileLocation = applicationProp.getProperty(AnniversaryConstants.LOCAL_IMAGE_DIR);
+			} else if(applicationProp.getProperty(AnniversaryConstants.DEPLOYMENT_TYPE).equalsIgnoreCase("server")) {
+				imageFileLocation = applicationProp.getProperty(AnniversaryConstants.SERVER_IMAGE_DIR);
+			} else {
+				System.out.println("[ERROR] Specify correct deployment type in application.properties");
+			}
+			
+			imageStartNum = new Integer(applicationProp.getProperty(AnniversaryConstants.IMAGE_START_INDEX)).intValue();
+			imageEndNum = new Integer(applicationProp.getProperty(AnniversaryConstants.IMAGE_END_INDEX)).intValue();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			
+			if(input!=null) {
+				
+				try{
+					input.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		int fileNum = randInt(imageStartNum, imageEndNum);
+				
+		String fileName = imageFileLocation + new Integer(fileNum).toString() + ".png";
 		return fileName;
 	}
 	
